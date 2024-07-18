6e8ac8fc2cc7cc51b246068e84f443c8d8b07ebc
Jacob Roman <jacob.roman@nttdata.com>
2024-07-18 15:03:13 -0400

    added mocks and fixed tests

---
+++ api/account_test.go
@@ -0,0 +1,43 @@
+package api
+
+import (
+    "testing"
+    "github.com/golang/mock/gomock"
+    mockdb "github.com/techschool/simplebank/db/mock"
+    db "github.com/techschool/simplebank/db/sqlc"
+    "github.com/techschool/simplebank/util"
+    "github.com/stretchr/testify/require"
+    "net/http"
+    "net/http/httptest"
+    "fmt"
+)
+
+func TestGetAccountAPI(t *testing.T) {
+    account := randomAccount()
+
+    ctrl := gomock.NewController(t)
+    defer ctrl.Finish()
+
+    store := mockdb.NewMockStore(ctrl)
+    store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(account.ID)).Times(1).Return(account, nil)
+
+    server := NewServer(store)
+    recorder := httptest.NewRecorder()
+
+    url := fmt.Sprintf("/accounts/%d", account.ID)
+    request, err := http.NewRequest(http.MethodGet, url, nil);
+    require.NoError(t, err)
+
+    server.router.ServeHTTP(recorder, request)
+
+    require.Equal(t, http.StatusOK, recorder.Code)
+}
+
+func randomAccount() db.Account {
+    return db.Account{
+        ID:      util.RandomInt(1, 1000),
+        Owner:    util.RandomOwner(),
+        Balance:  util.RandomMoney(),
+        Currency: util.RandomCurrency(),
+    }
+}
--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,6 +6,7 @@ import (
     "os"
     "testing"
 
+    "github.com/techschool/simplebank/util"
     _ "github.com/lib/pq"
 )
 
@@ -13,12 +14,12 @@ var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-    config, err := LoadConfig("../../..")
+    config, err := util.LoadConfig("../..")
     if err != nil {
         log.Fatal("cannot load config:", err)
     }
 
-    testDB, err = sql.Open(config,DBDriver, config.DBSource)
+    testDB, err = sql.Open(config.DBDriver, config.DBSource)
     if err != nil {
         log.Fatal("cannot connect to db:", err)
     }
